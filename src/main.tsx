725f5ab21f539f1211ac0b5822117b7832f49a2d
Aayush Sharma <aayushsharma47163@gmail.com>
2023-07-15 21:00:15 +0530

    added (1)

--- src/main.tsx
+++ src/main.tsx
@@ -23,6 +23,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+     <RouterProvider router={router} >
+      <App />
+     </RouterProvider>
   </React.StrictMode>,
 )
